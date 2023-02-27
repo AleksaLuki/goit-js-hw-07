f95831c252ecdc1d8b13d663106eeb360c8b86fb
AleksaLuki <dimirtiy9000@gmail.com>
2023-02-27 11:20:04 +0200

    lightbox07
    lightbox07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,13 +5,13 @@ console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createMarkup(galleryItems);
+
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
 galleryContainer.addEventListener('click', ImgClick);
 
 
 function createMarkup(items) {
-  return items
-    .map(({ preview, original, description }) => {
+  return items.map(({ preview, original, description }) => {
       return `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -55,27 +55,5 @@ const instance = basicLightbox.create(
 
 
 
-
-//   import * as basicLightbox from 'basiclightbox'
-
-// const instance = basicLightbox.create(`
-//     <img src="assets/images/image.png" width="800" height="600">
-// `)
-
-// instance.show()
-
-// const instance = basicLightbox.create(`
-//     <div class="modal">
-//         <p>A custom modal that has been styled independently. It's not part of basicLightbox, but perfectly shows its flexibility.</p>
-//         <input placeholder="Type something">
-//         <a>Close</a>
-//     </div>
-// `, {
-//     onShow: (instance) => {
-//         instance.element().querySelector('a').onclick = instance.close
-//     }
-// })
-
-// instance.show()
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,23 +5,24 @@ console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 
-const markup = galleryItems.reduce(
-    (acc, { original, preview, description }) =>
-      (acc += `<li>
-    <a class="gallery__item" href="${original}">
-      <img
-        class="gallery__image"
-        src="${preview}"
-        alt="${description}"
-      />
-    </a>
-  </li>`),
-    ''
-  );
-  
-  galleryContainer.insertAdjacentHTML('beforeend', markup);
-  
-  const lightbox = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-  });
+const itemsMarkup = createGalleryItemsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+
+function createGalleryItemsMarkup(items) {
+  return items.map(({ preview, original, description }) => {
+    return `<li>
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
